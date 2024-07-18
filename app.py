666cb35b75551165e83b3987d409adee244c7227
brian tran <btran@hamilton.edu>
2024-07-18 10:05:00 -0700

    with cors

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+import sys
 from pyplate import Substance, Container, Plate, Recipe
 
+
 app = Flask(__name__)
+cors = CORS(app)
+app.config["CORS_HEADERS"] = "Content-Type"
+
+# CORS(app, origins=["http://localhost:3000"])
 
 
 @app.route("/")
@@ -34,3 +41,48 @@ def foo():
     plate = results[plate.name]
 
     return str(plate.get_volumes(unit="uL"))
+
+
+@app.route("/bar", methods=["POST"])
+def bar():
+    data = request.json
+    for x in data:
+        print(x, file=sys.stderr)
+
+    return jsonify(data)
+
+
+@app.route("/test", methods=["POST"])
+def test():
+    data = request.json
+
+    objs = {}
+    for obj in data["preparations"]:
+        objs[obj["name"]] = create_obj(obj)
+
+    for name in objs:
+        print(objs[name].is_solid(), file=sys.stderr)
+
+    return jsonify(data)
+
+
+def create_obj(obj):
+    name = obj["name"]
+    match obj["type"]:
+
+        case "Solid":
+            mol_weight = float(obj["molweight"])
+            return Substance.solid(name=name, mol_weight=mol_weight)
+
+        case "Liquid":
+            mol_weight = float(obj["molweight"])
+            density = float(obj["density"])
+            return Substance.liquid(
+                name=name,
+                mol_weight=mol_weight,
+                density=density,
+            )
+
+        case "Enzyme":
+            specific_activity = obj["activity"]
+            return Substance.enzyme(name=name, specific_activity=specific_activity)
